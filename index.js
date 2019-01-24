b8fe6fa3cd191f1823296cb7c6733abd120e5b03
Timur Z <engineer@timurz.com>
2019-01-24 16:09:46 +0600

    add cors headers

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ exports.handler = (event, context, callback) => {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
