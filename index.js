228e5258aba882f9e0d71c70617068b37b99c243
Timur Z <engineer@timurz.com>
2019-01-24 16:43:18 +0600

    test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ exports.handler = (event, context, callback) => {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST,OPTIONS',
+          'Access-Control-Allow-Methods': 'POST, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
           'Content-Type': 'application/json',
         },
